cf726ae3338486b5fc09a6ac796ab7f5184cff56
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-28 11:58:02 +0530

    Create main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,23 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr_block
+  enable_dns_support = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+}
+
+resource "aws_security_group" "eks_sg" {
+  name        = "eks-sg"
+  description = "Allow all inbound traffic to the EKS cluster"
+  vpc_id      = aws_vpc.main.id
+}

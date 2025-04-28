6c7468b990910f7631ec683ddcde5317a435851f
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-28 11:46:14 +0530

    Create variables.tf

---
+++ modules/app/variables.tf
@@ -0,0 +1,14 @@
+variable "cluster_name" {
+  description = "EKS Cluster name"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs"
+  type        = list(string)
+}

053112512b2c77a993db4abfd5aad0e9074e7cd7
Pallavi-1104 <tarkeshubham2006@gmail.com>
2025-04-28 11:54:35 +0530

    Create main.tf

---
+++ modules/monitoring/main.tf
@@ -0,0 +1,25 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  namespace  = "monitoring"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  depends_on = [module.eks]
+
+  values = [file("${path.module}/prometheus-values.yaml")]
+}
+
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  namespace  = "monitoring"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+
+
+  values = [file("${path.module}/grafana-values.yaml")]
+}
